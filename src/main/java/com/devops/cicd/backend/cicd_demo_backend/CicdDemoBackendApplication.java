d894eeb67a184fa61790a24ee363c324351011cb
pawanhb <pawanhb21@gmail.com>
2025-04-16 00:16:07 +0530

    first commit

---
+++ src/main/java/com/devops/cicd/backend/cicd_demo_backend/CicdDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devops.cicd.backend.cicd_demo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/cicd/backend/cicd_demo_backend/CicdDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.cicd.backend.cicd_demo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
